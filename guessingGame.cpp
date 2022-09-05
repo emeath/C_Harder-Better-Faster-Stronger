be8a2ecc71660a59c8e93b783ce5e4a02f8323ab
emeath <matheus.martins.jeronimo@gmail.com>
2022-09-04 21:16:54 -0300

    adds logic and gets input from user

--- guessingGame.cpp
+++ guessingGame.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 using namespace std;
-int main() {
+int main()
+{
 
-cout << "#################################" << endl;
-cout << "# Welcome to the guessing game! #" << endl;
-cout << "#################################" << endl;
+    cout << "#################################" << endl;
+    cout << "# Welcome to the guessing game! #" << endl;
+    cout << "#################################" << endl;
+
+    int secret_number = 42;
+    
+    int guess;
+    cout << "Guess a number: ";
+    cin >> guess;
+    cout << "You guessed: " << guess << endl;
+
+    if (guess == secret_number)
+    {
+        cout << "Congratulation! You guessed correctly the secret number!" << endl;
+    }
+    else if (guess > secret_number) {
+        cout << "Your guess is biggger then the secret number!" << endl;
+    }
+    else {
+        cout << "Your guess is smaller then the secret number!" << endl;
+    }
 }
