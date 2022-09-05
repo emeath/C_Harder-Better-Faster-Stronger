1d8c2df2c481bbd533d5da180531480c63a87342
emeath <matheus.martins.jeronimo@gmail.com>
2022-09-04 22:35:33 -0300

    adds map/dictionary data structure

---
+++ hangman_game/README.md
@@ -0,0 +1,19 @@
+# C_Harder-Better-Faster-Stronger
+
+## Hang man game
+
+This mini project uses the c++ 11 standard.
+
+To make it easy to compile the code we are using MakeFile.
+
+To compile using it, run:
+
+```shell
+make hangmanGame
+```
+
+To run it:
+
+```shell
+./hangmanGame
+```
--- hangman_game/hangmanGame.cpp
+++ hangman_game/hangmanGame.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 #include <string>
+#include <map>
+
 using namespace std;
 
 const string SECRET_WORD = "BANANA";
+map<char, bool> letter_already_guessed;
 
 bool letter_exists(char guess)
 {
-    for (int i = 0; i < SECRET_WORD.size(); i++)
+    for (char letter : SECRET_WORD)
     {
-        if (guess == SECRET_WORD[i])
+        if (guess == letter)
         {
             return true;
         }
     }
+
     return false;
 }
+
+
 
 int main()
 {
@@ -25,12 +31,28 @@ int main()
 
     while (not_right_answer && not_hanged)
     {
+        for (char letter : SECRET_WORD)
+        {
+            if (letter_already_guessed[letter])
+            {
+                cout << letter << " ";
+            }
+            else
+            {
+                cout << "_ ";
+            }
+        }
+        cout << endl;
+
         char guess;
         cin >> guess;
         if (letter_exists(guess))
         {
             cout << "Correct guess!" << endl;
-        } else {
+            letter_already_guessed[guess] = true;
+        }
+        else
+        {
             cout << "Wrong guess!" << endl;
         }
     }
