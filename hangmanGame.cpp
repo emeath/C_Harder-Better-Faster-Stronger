5c2a13ff34b255d3869fe7848c9188d3604fb0a7
emeath <matheus.martins.jeronimo@gmail.com>
2022-09-04 22:12:12 -0300

    adds verification if letter exists in secret word

--- hangmanGame.cpp
+++ hangmanGame.cpp
@@ -2,8 +2,36 @@
 #include <string>
 using namespace std;
 
+const string SECRET_WORD = "BANANA";
+
+bool letter_exists(char guess)
+{
+    for (int i = 0; i < SECRET_WORD.size(); i++)
+    {
+        if (guess == SECRET_WORD[i])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main()
 {
-    string secret_word = "BANANA";
-    cout << secret_word << endl;
+    cout << SECRET_WORD << endl;
+
+    bool not_right_answer = true;
+    bool not_hanged = true;
+
+    while (not_right_answer && not_hanged)
+    {
+        char guess;
+        cin >> guess;
+        if (letter_exists(guess))
+        {
+            cout << "Correct guess!" << endl;
+        } else {
+            cout << "Wrong guess!" << endl;
+        }
+    }
 }
