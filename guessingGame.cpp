be9560b31e205778dec234a8bc286d1842e93597
emeath <matheus.martins.jeronimo@gmail.com>
2022-09-04 21:52:41 -0300

    adds difficulty level

--- guessingGame.cpp
+++ guessingGame.cpp
@@ -9,6 +9,26 @@ int main()
     cout << "# Welcome to the guessing game! #" << endl;
     cout << "#################################" << endl;
 
+    cout << "Chose the difficulty level: " << endl;
+    cout << "(E)asy (M)edium (H)ard" << endl;
+
+    char difficulty;
+    cin >> difficulty;
+    int number_of_chances;
+
+    if (difficulty == 'E')
+    {
+        number_of_chances = 15;
+    }
+    else if (difficulty == 'M')
+    {
+        number_of_chances = 10;
+    }
+    else
+    {
+        number_of_chances = 5;
+    }
+
     const int SECRET_NUMBER = 42;
     bool did_not_got_it_right = true;
     int guess;
@@ -16,11 +36,11 @@ int main()
 
     double score = 1000.0;
 
-    while (did_not_got_it_right)
+    for(attempts = 1; attempts < number_of_chances; attempts++)
     {
+        cout << "Attempt #" << attempts << endl;
         cout << "Guess a number: ";
         cin >> guess;
-        cout << "You guessed: " << guess << endl;
 
         double lost_points = abs(guess - SECRET_NUMBER) / 2.0;
         score -= lost_points;
@@ -32,6 +52,7 @@ int main()
         {
             cout << "Congratulation! You guessed correctly the secret number!" << endl;
             did_not_got_it_right = false;
+            break;
         }
         else if (is_bigger)
         {
@@ -41,15 +62,21 @@ int main()
         {
             cout << "Your guess is smaller then the secret number!" << endl;
         }
-
-        attempts++;
     }
 
     cout << "GAME OVER!" << endl;
-    cout << "You got it right in " << attempts << " attemps!" << endl;
-    
-    cout.precision(2);
-    cout << fixed;
-    
-    cout << "You scored " << score << " points!" << endl;
+
+    if (did_not_got_it_right)
+    {
+        cout << "You lose! Try again!" << endl;
+    }
+    else
+    {
+        cout << "You got it right in " << attempts << " attemps!" << endl;
+
+        cout.precision(2);
+        cout << fixed;
+
+        cout << "You scored " << score << " points!" << endl;
+    }
 }
