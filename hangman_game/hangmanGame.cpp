679e6b49d74a0282cc00b19c36b74fdd4d2c9bcf
emeath <matheus.martins.jeronimo@gmail.com>
2022-09-04 23:00:55 -0300

    refactors code

--- hangman_game/hangmanGame.cpp
+++ hangman_game/hangmanGame.cpp
@@ -9,7 +9,6 @@ const string SECRET_WORD = "BANANA";
 map<char, bool> letter_already_guessed;
 vector<char> wrong_guesses;
 
-
 bool letter_exists(char guess)
 {
     for (char letter : SECRET_WORD)
@@ -23,56 +22,98 @@ bool letter_exists(char guess)
     return false;
 }
 
-
-
-int main()
+bool not_guessed_all_letter_right()
 {
+    for (char letter : SECRET_WORD)
+    {
+        if (!letter_already_guessed[letter])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool not_hanged()
+{
+    return wrong_guesses.size() < 5;
+}
+
+void prints_header()
+{
+
     cout << "+++++++++++++++++++++++++++++++++++" << endl;
     cout << "+          Hangman Game           +" << endl;
     cout << "+++++++++++++++++++++++++++++++++++" << endl;
     cout << endl;
+}
 
-    cout << SECRET_WORD << endl;
-
-    bool not_right_answer = true;
-    bool not_hanged = true;
-
-    while (not_right_answer && not_hanged)
+void prints_errors()
+{
+    cout << "Wrong guesses: ";
+    for (char letter : wrong_guesses)
     {
-        cout << "Wrong guesses: ";
-        for(char letter : wrong_guesses) {
+        cout << letter << " ";
+    }
+    cout << endl;
+}
+
+void prints_word()
+{
+    for (char letter : SECRET_WORD)
+    {
+        if (letter_already_guessed[letter])
+        {
             cout << letter << " ";
-        }
-        cout << endl;
-
-        for (char letter : SECRET_WORD)
-        {
-            if (letter_already_guessed[letter])
-            {
-                cout << letter << " ";
-            }
-            else
-            {
-                cout << "_ ";
-            }
-        }
-        cout << endl;
-
-        cout << "Your guess: ";
-
-        char guess;
-        cin >> guess;
-        if (letter_exists(guess))
-        {
-            cout << "Correct guess!" << endl;
-            letter_already_guessed[guess] = true;
         }
         else
         {
-            cout << "Wrong guess!" << endl;
-            wrong_guesses.push_back(guess);
+            cout << "_ ";
         }
+    }
+    cout << endl;
+}
 
-        cout << endl;
+void player_guesses()
+{
+    cout << "Your guess: ";
+
+    char guess;
+    cin >> guess;
+    if (letter_exists(guess))
+    {
+        cout << "Correct guess!" << endl;
+        letter_already_guessed[guess] = true;
+    }
+    else
+    {
+        cout << "Wrong guess!" << endl;
+        wrong_guesses.push_back(guess);
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    prints_header();
+
+    while (not_guessed_all_letter_right() && not_hanged())
+    {
+        prints_errors();
+        prints_word();
+        player_guesses();
+    }
+
+    cout << "GAME OVER!" << endl;
+    cout << "The secret word is: " << SECRET_WORD << endl;
+
+    if (not_guessed_all_letter_right())
+    {
+        cout << "You lose! Try again!" << endl;
+    }
+    else
+    {
+        cout << "Congratulation! You guessed the secret word!" << endl;
     }
 }
